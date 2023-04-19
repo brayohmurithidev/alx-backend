c59b4529186d15a6e56d0eab7d04f3ccf411c980
brayohmurithidev <murithibrianm@gmail.com>
2023-04-19 14:31:57 +0300

    returned on the put method

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -17,6 +17,7 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         if key is not None and item is not None:
             self.cache_data[key] = item
+            return item
 
     def get(self, key):
         if key is None or key not in self.cache_data:
