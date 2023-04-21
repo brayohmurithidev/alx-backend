384379c7770462c375ba44fa6c92fbdca82fa3b0
brayohmurithidev <murithibrianm@gmail.com>
2023-04-21 18:42:08 +0300

    fifo caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+'''FIFO Caching'''
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    '''Implents deleting elements in cache based on FIFO algorithm'''
+    data_keys = []
+
+    def __init__(self):
+        # grab the parent constructor method
+        super().__init__()
+
+    def put(self, key, item):
+        '''adds items to the cache_data'''
+        if key is None and item is None:
+            return
+        else:
+            num = len(self.cache_data)
+            if num >= BaseCaching.MAX_ITEMS:
+                fkey = next(iter(self.cache_data))
+                del self.cache_data[fkey]
+                print(f'DISCARD: {fkey}')
+            self.cache_data[key] = item
+            return item
+
+    def get(self, key):
+        """Fetch cache data"""
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
